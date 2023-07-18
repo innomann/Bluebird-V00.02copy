7af3fd219ff8aacbdf0e8711bcba392e755ad968
Steven Bira <stevenbira@Stevens-MacBook-Air.local>
2023-07-18 15:26:39 -0700

    Blog v0.00#

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+require("dotenv").config(); // for loading environment variables
+const bodyParser = require("body-parser");
+const passport = require("passport")
+const mongoose = require("mongoose");
+require("dotenv").config()
+const path = require("path");
+const users = require("./routes/users")
+const posts = require("./routes/posts")
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+// Connect to MongoDB
+mongoose.connect(db,{ useNewUrlParser: true })
+.then(() => console.log("MongoDB successfully connected"))
+.catch(err => console.log(err));
+
+
+app.use(passport.initialize());
+require("./middleware/passport") (passport)
+app.use("/api/users", users);
+app.use("/api/posts/", posts);
+
+app.get("/", (req, res) => {
+    console.log(req.body)
+  res.send("helloo");
+});
+app.listen(PORT, () => {
+console.log(`Server up and running on port ${PORT}`);
+});
